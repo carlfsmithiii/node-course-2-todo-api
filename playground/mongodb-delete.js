684a804c1165bae952d2cbcd86ae1e9e0b57ac8f
Carl Smith <carlfsmithiii@gmail.com>
2018-10-16 15:28:42 -0400

    practicing deleting from mongodb

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,35 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true}, (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // deleteMany
+    // db.collection('Todos').deleteMany({test: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // });
+
+    // deleteOne
+    // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // })
+
+
+    // findOneAndDelete
+    // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').deleteMany({name: 'Carl'}).then((result) => {
+        console.log(result);
+    });
+
+    db.collection('Users').findOneAndDelete({_id: new ObjectID('5bc6288f335bed19cfed7f02')}).then((result) => {
+        console.log(JSON.stringify(results, undefined, 2));
+    });
+
+    //db.close();
+});
