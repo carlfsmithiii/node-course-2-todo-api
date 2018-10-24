2fa1bfc01f89708dc838323f2779afb7fad33bdd
Carl Smith <carlfsmithiii@gmail.com>
2018-10-24 17:44:43 -0400

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const {
     ObjectID
 } = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 const {
     mongoose
@@ -131,6 +132,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((err) => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
