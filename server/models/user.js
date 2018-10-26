2cc6bc2a461ba9f33ee3f2037abd2fa31e5281cb
Carl Smith <carlfsmithiii@gmail.com>
2018-10-26 09:41:27 -0400

    Update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -42,10 +42,9 @@ UserSchema.methods.toJSON = function() {
 UserSchema.methods.generateAuthToken = function() {
     const user = this;
     const access = 'auth';
-    const token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+    const token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
     user.tokens = user.tokens.concat([{access, token}]);
-    // user.tokens.push({access, token});
 
     return user.save().then(() => {
         return token;
@@ -67,7 +66,7 @@ UserSchema.statics.findByToken = function(token) {
     let decoded;
 
     try {
-        decoded = jwt.verify(token, 'abc123');
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
     } catch (err) {
         return Promise.reject();
     }
@@ -107,10 +106,6 @@ UserSchema.pre('save', function(next) {
                 user.password = hash;
                 next();
             });
-        // }).catch((err) => {
-        //     console.log('04');
-        //     next();
-        // });
         });
     } else {
         next();
